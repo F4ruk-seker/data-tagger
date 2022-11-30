d0c9576330e8e98a670b0af819892be207d32f21
F4ruk-seker <ahmetfaruk731@gmail.com>
2022-11-30 05:06:46 +0300

    add Meta data viewport For mobil devices

--- loadKReview.py
+++ loadKReview.py
@@ -4,13 +4,17 @@ from DataManager.models import Reviews
 import json
 
 table = Reviews.objects.create(
-    name='Test köfteci yusuf',
-    explanation='test'
+    name='köfteci yusuf',
+    explanation='Google maps dan alınan yorumlar'
 )
 tyt = open('review.json','r')
 tytData = json.load(tyt)
 
 # repeating data
-
+print('START')
+counter = 0
 for _ in tytData:
+    counter += 1
+    print(counter)
     table.comments.create(comment=_.get('text'),rate=_.get('rate'))
+print('END')

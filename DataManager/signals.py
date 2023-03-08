a2f191b48d8f26b8e5b3e5fbd9a9e1354884db17
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-03-08 09:30:24 +0300

    fix bug: accept negative values

--- DataManager/signals.py
+++ DataManager/signals.py
@@ -2,11 +2,8 @@ from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from DataManager.models import Reviews
 
+
 @receiver(pre_delete, sender=Reviews)
 def reviews_post_delete(sender, instance, **kwargs):
-    print("signal")
     reviews = Reviews.objects.get(id=instance.id)
     reviews.comments.all().delete()
-    # reviews.comments.all().delete()
-    # Özel işlemlerinizi burada gerçekleştirin
-    pass
--- loadKReview.py
+++ loadKReview.py
@@ -3,30 +3,34 @@ from DataManager.models import Reviews
 from DataManager.models import Tag
 
 import json
-
+#
 table = Reviews.objects.create(
     name='Yorumlar',
-    explanation='yorumlar'
+    explanation='fixing : utf-8-tr'
 )
-tyt = open('comments.json','r',encoding='utf-8')
-tytData = json.load(tyt)
+reviews = open('comments.json','r',encoding='utf-8')
+reviews = json.load(reviews)
 
 # repeating data
-print('START')
+print(f'{"#"*25} - START - {"#"*25}')
 counter = 0
-for _ in tytData:
+remaining_count = len(reviews)
+for _ in reviews:
     counter += 1
-    print(counter)
 
     tag_name = _.get('result')
     comment = _.get('comment')
-    if tag_name == 'YETKÄ°NLÄ°K':
-        tag_name = 'YETKİNLİK'
-    if tag_name == 'HEYECAN':
-        tag_name = 'HEYCAN'
-    print(tag_name)
-    tag = Tag.objects.get(name=tag_name)
 
+    if len(str(counter))==1:
+        print(f'| 0000{counter}/{remaining_count} | @ {comment[:43]}')
+    if len(str(counter))==2:
+        print(f'| 00{counter}/{remaining_count} | @ {comment[:43]}')
+    if len(str(counter))==3:
+        print(f'| 0{counter}/{remaining_count} | @ {comment[:43]}')
+    if len(str(counter))>=4:
+        print(f'| {counter}/{remaining_count} | @ {comment[:43]}')
+    #
+    tag = Tag.objects.get(name=tag_name)
     table.comments.create(comment=comment,tag=tag)
 
-print('END')
+print(f'{"#"*25} - END - {"#"*25}')
